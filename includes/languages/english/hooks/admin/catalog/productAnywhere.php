100a3489d7ec4a36fb1ca771f3b2387c67ed0005
gburton <gb8231059@gmail.com>
2026-07-31 12:57:45 +0100

    Admin Sharing
    Some shopowners might not want a public facing "share system", so this brings sharing links into the admin area.   Or shopowner might be using Product Anywhere for their own needs rather than allowing public.
    
    Shopowner would manually need to let users know the relevant code.

---
+++ includes/languages/english/hooks/admin/catalog/productAnywhere.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2026 Phoenix Cart
+
+  Released under the GNU General Public License
+*/
+
+const ANYWHERE_LABEL = 'Product Anywhere Embed';
+
+const ANYWHERE_COPY = '<i class="fas fa-copy me-1"></i> Copy Code';
+const ANYWHERE_COPIED = '<i class="fas fa-check me-1"></i> Copied!';
